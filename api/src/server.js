4e9b35a553383d4675502fa5215272b67c0916aa
Shashank Garg <shashank.garg@cars24.com>
2025-07-12 21:40:14 +0530

    server.js changes

--- api/src/server.js
+++ api/src/server.js
@@ -3,10 +3,14 @@ const express = require('express');
 const createApp = async () => {
   const app = express();
 
-  app.get('/health', (req, res) => {
+  const api = express.Router();
+
+  api.get('/health', (req, res) => {
     console.log("✅ /health hit");
     res.status(200).send("OK");
   });
+
+  app.use('/api/v1', api);
 
   return app;
 };
