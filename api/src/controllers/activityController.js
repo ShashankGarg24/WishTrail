f370450960705e75a6ead5917f0ab125298fb616
Shashank Garg <shashank.garg@cars24.com>
2025-07-14 17:31:02 +0530

    redis key modified for activity

--- api/src/controllers/activityController.js
+++ api/src/controllers/activityController.js
@@ -41,7 +41,8 @@ const getRecentActivities = async (req, res, next) => {
       });
     }
     
-    await redisClient.setex(cacheKey, CACHE_TTL, JSON.stringify(activities));
+    const safeActivities = JSON.parse(JSON.stringify(activities));
+    await redisClient.setex(cacheKey, CACHE_TTL, JSON.stringify(safeActivities));
 
     res.status(200).json({
       success: true,
