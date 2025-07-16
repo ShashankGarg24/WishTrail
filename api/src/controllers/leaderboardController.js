481578dd117b3ba22f06befb9ec7403c7abc7ecf
Shashank Garg <shashank.garg@cars24.com>
2025-07-17 00:48:04 +0530

    Friends leaderboard fix

--- api/src/controllers/leaderboardController.js
+++ api/src/controllers/leaderboardController.js
@@ -503,8 +503,7 @@ const getFriendsLeaderboard = async (req, res, next) => {
     
     // Get user's following list
     const following = await Follow.getFollowing(req.user.id);
-    console.log(following)
-    const followingIds = following.map(f => f._id);
+    const followingIds = following.map(f => f.followingId);
     
     // Include current user in the leaderboard
     followingIds.push(req.user.id);
